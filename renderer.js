99792a29f52c1cd2ef166814efa7da44ec9604f4
Stoyan <slownessbg@gmail.com>
2021-03-30 23:44:59 +0300

    Optimizing code and making progress ot matchlist

--- renderer.js
+++ renderer.js
@@ -2,17 +2,18 @@ let searchBtn = document.querySelector('#search-btn');
 
 searchBtn.addEventListener('click', fetchProfile);
 
-window.addEventListener('load', () => {
-    Endpoints.ApiKey = document.body.getAttribute('data-api-key');
-    searchBtn.removeAttribute('disabled');
-});
-
 let queueNames = {
-    "RANKED_SOLO_5x5": "Solo/Duo",
-    "RANKED_FLEX_SR": "Flex",
+    'RANKED_SOLO_5x5': 'Solo/Duo',
+    'RANKED_FLEX_SR': 'Flex',
 }
 
+let apiKey;
+
 function fetchProfile() {
+    if (!apiKey) {
+        apiKey = document.body.getAttribute('data-api-key');
+    }
+
     let name = document.querySelector('#input-name').value;
 
     fetch(Endpoints.SummonerV4ByName(name))
@@ -25,51 +26,92 @@ function fetchProfile() {
             fetch(Endpoints.LeagueV4BySummoner(summoner.id))
                 .then(response => response.json())
                 .then(queues => {
-
                     let rankedWrapper = document.querySelector('.rank-wrapper')
-                    queues.forEach((queue) => {
+                    rankedWrapper.innerHTML = '';
+
+                    if (queues.length == 0) {
                         let rankedInfo = document.createElement('div');
-                        rankedInfo.className = "rank-info";
+                        rankedInfo.className = 'rank-info';
                         rankedWrapper.appendChild(rankedInfo);
 
-                        let rankedQueue = document.createElement('div');
-                        rankedQueue.className = "rank-queue";
-                        rankedQueue.innerText = queueNames[queue.queueType];
-                        rankedInfo.appendChild(rankedQueue);
+                        rankedInfo.innerHTML = 
+                        `
+                        <img width="100" height="100" src="${Endpoints.OPGGTierImageDefault}"/>
+                        <div class="rank-text">UNRANKED</div>
+                        `;
+                    }
+                    
+                    queues.forEach((queue) => {
+                        let rankedInfo = document.createElement('div');
+                        rankedInfo.className = 'rank-info';
+                        rankedWrapper.appendChild(rankedInfo);
 
-                        let rankedImage = document.createElement('img');
-                        rankedImage.height = "100";
-                        rankedImage.width = "100";
-                        rankedImage.src = Endpoints.OPGGTierImage(queue.tier.toLowerCase());
-                        rankedInfo.appendChild(rankedImage);
-
-                        let rankedText = document.createElement('div');
-                        rankedText.className = "rank-text";
-                        rankedText.innerText = queue.tier + " " + queue.rank;
-                        rankedInfo.appendChild(rankedText);                        
-                        
-                        
+                        rankedInfo.innerHTML = 
+                        `
+                        <div class="rank-queue">${queueNames[queue.queueType]}</div>
+                        <img width="100" height="100" src="${Endpoints.OPGGTierImage(queue.tier.toLowerCase())}"/>
+                        <div class="rank-text">${queue.tier} ${queue.rank}</div>
+                        `;
                     })
-                })
+                });
+            
+            fetch(Endpoints.MatchV4Matchlist(summoner.accountId))
+                .then(response => response.json())
+                .then(matchList => {
+                    let matchesWrapper = document.querySelector('.matches-wrapper');
+
+                    matchList.matches.forEach(async function(match) {
+                        let game = await fetch(Endpoints.MatchV4MatchById(match.gameId)).then(response => response.json())
+                        let participantIdentity = game.participantIdentities.find(identity => identity.player.summonerId == summoner.id);
+                        let participant = game.participants.find(identity => identity.participantId == participantIdentity.participantId);
+                        let team = game.teams.find(team => team.teamId == participant.teamId);
+
+                        console.log('-GAME-');
+                        console.log(game);
+                        console.log(participantIdentity);
+                        console.log(participant);
+                        console.log(team);
+                        console.log('-/GAME-');
+                        
+                        let matchDiv = document.createElement('div');
+                        matchDiv.className = 'match';
+                        matchesWrapper.appendChild(matchDiv);
+
+                        matchDiv.innerHTML =
+                        `
+                        <span>${team.win}</span>
+                        `
+                    });
+                });
         });
 }
 
 class Endpoints {
-    static ApiKey = null;
+    static baseUrl = 'https://eun1.api.riotgames.com/lol';
 
     static SummonerV4ByName(summonerName) {
-        return 'https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=' + Endpoints.ApiKey;
+        return `${this.baseUrl}/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`;
     }
 
     static LeagueV4BySummoner(summonerId) {
-        return 'https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summonerId + '?api_key=' + Endpoints.ApiKey;
+        return `${this.baseUrl}/league/v4/entries/by-summoner/${summonerId}?api_key=${apiKey}`;
+    }
+
+    static MatchV4Matchlist(accountId) {
+        return `${this.baseUrl}/match/v4/matchlists/by-account/${accountId}?endIndex=10&begindIndex=0&api_key=${apiKey}`;
+    }
+
+    static MatchV4MatchById(gameId) {
+        return `${this.baseUrl}/match/v4/matches/${gameId}?api_key=${apiKey}`;
     }
 
     static DDragonProfileIcon(profileIconId) {
-        return 'http://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/' + profileIconId + '.png';
+        return `http://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/${profileIconId}.png`;
     }
 
     static OPGGTierImage(tier) {
-        return 'https://opgg-static.akamaized.net/images/medals/' + tier + '_1.png';
+        return `https://opgg-static.akamaized.net/images/medals/${tier}_1.png`;
     }
+
+    static OPGGTierImageDefault = 'https://opgg-static.akamaized.net/images/medals/default.png';
 }
