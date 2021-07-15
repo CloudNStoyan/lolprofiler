1385622f3d634d51f322ad2d4f874ae6671d5911
Stoyan <slownessbg@gmail.com>
2021-07-15 23:59:17 +0300

    adding showall to logger

--- logger.js
+++ logger.js
@@ -12,5 +12,13 @@ let logger = {
             let logs = this.channels[channel]
             logs.forEach(log => console.log(log))
         });
+    },
+    showAll() {
+        let channels = Object.keys(this.channels).map(ch => channels[ch])
+        this.channels.forEach(channel => {
+            let logs = this.channels[channel]
+            console.log(`%c${channel}`, 'color: purple; font-size: 20px')
+            logs.forEach(log => console.log(log))
+        });
     }
 }
